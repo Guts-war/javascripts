24da7934ab351775de6c81142a430b2d703b80e6
Guts-war <gutswar0@gmail.com>
2025-07-16 09:04:03 -0300

    up

---
+++ aula11/ex007.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+console.log('Sempre coloque o cinto de segurança')
+ 
+if (vel > 60) {
+    console.log('Velocidade a cima da permissão. Mutado!')
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var country = 'Japão'
+console.log(`Você esta em ${country}`)
+if (country == 'Brasil') {
+    console.log('Você é brasileiro!')
+} else {
+    console.log('Você é estrangeiro')
+}
